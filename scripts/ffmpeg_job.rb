541cf1e5ad7e35ab65c0a5a6092d99aff9b3c440
yang <yang@yangdeMacBook-Pro.local>
2016-03-04 02:10:08 +0800

    Use open 3

--- scripts/ffmpeg_job.rb
+++ scripts/ffmpeg_job.rb
@@ -1,4 +1,6 @@
 require 'redis'
+require 'open3'
+require 'json'
 
 redis = Redis.new
 # redis.lpush
@@ -8,8 +10,11 @@ loop do
   job = redis.rpop('ffmpeg_job_queue')
   continue if job.nil?
   thr = Thread.new {
-    msg = `#{job}`
+    Open3.popen3(job) {|stdin, stdout, stderr, wait_thr|
+      pid = wait_thr.pid # pid of the started process.
+      redis.lpush('ffmpeg_jobing_queue', {pid: pid, cmd: job}.to_json)
+      exit_status = wait_thr.value # Process::Status object returned.
+    }
   }
-  threads << thr
+  threads << {thread: thr, pid: @pid}
 end
-
