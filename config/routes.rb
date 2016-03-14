49a9018cb5cdf5a383a03c21ae0ed18ffe50f968
yang <yang@yangdeMacBook-Pro.local>
2016-03-14 17:07:26 +0800

    Clear routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -22,13 +22,6 @@ Rails.application.routes.draw do
     end
   end
 
-  # sidekiq
-  # require 'sidekiq/web'
-  # Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-  #   username == 'yang' && password == '123$%^'
-  # end
-  # mount Sidekiq::Web, at: '/sidekiq'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
